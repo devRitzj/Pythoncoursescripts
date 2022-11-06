8911b55359484dc80237298e958e2ee7d977f510
dev.ritujaiswal <dev.ritujaiswal@gmail.com>
2022-11-06 14:24:22 -0600

    first commit

---
+++ simpledivision.py
@@ -0,0 +1,6 @@
+#This code is supposed to take two numbers, divide one by another so that the result is equal to 1, and display the result on the screen. Unfortunately, there is an error in the code. Find the error and fix it, so that the output is correct.
+
+numerator = int(input("Enter the numerator:"))
+denominator = int(input ("Enter the denominator:"))
+result = numerator / denominator
+print("Result:", result)
